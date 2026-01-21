cf5c50975e7f1de7575901f60337150c9a423649
ramonperesdev <ramon.nobre.peres@hotmail.com>
2026-01-20 23:28:37 -0300

    Update Vite configuration to exclude specific directories and improve Playwright report script
    - Added exclusions for node_modules, dist, e2e, and various config files in vite.config.ts to streamline the build process.
    - Updated the Playwright report script in index.html to include a new base64 encoded zip file for improved test report handling.

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,12 @@ export default defineConfig({
     environment: 'happy-dom',
     globals: true,
     setupFiles: './src/test/setup.ts',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*'
+    ]
   },
 })
